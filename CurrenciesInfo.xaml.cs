951e02a7ed3ecbb6f5f41ab081e81619258fc320
Ihor Fedorchenko <invisiblepanda04@gmail.com>
2023-01-19 12:41:35 +0200

    markets links were added

--- CurrenciesInfo.xaml.cs
+++ CurrenciesInfo.xaml.cs
@@ -17,6 +17,7 @@ using Newtonsoft.Json;
 using CoinGecko.Clients;
 using System.Net.Http;
 using CoinGecko.Entities.Response.Coins;
+using CoinGecko.Entities.Response.Exchanges;
 
 namespace TestAssignment
 {
@@ -27,6 +28,7 @@ namespace TestAssignment
 
         private PingClient pingClient = new PingClient(httpClient, serializerSettings);
         private CoinsClient coinsClient = new CoinsClient(httpClient, serializerSettings);
+        private ExchangesClient exchangesClient = new ExchangesClient(httpClient, serializerSettings);
 
         public CurrenciesInfo()
         {
@@ -40,6 +42,24 @@ namespace TestAssignment
                 CurrenciesListAll.ItemsSource = coinsClient.GetCoinMarkets("USD").Result.ToList();
                 CurrenciesListAll.SelectedIndex = 0;
                 CurrenciesListAll.Focus();
+            }
+        }
+
+        private async void CurrenciesListAll_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if ((await pingClient.GetPingAsync()).GeckoSays != String.Empty)
+            {
+                CoinMarkets selectedCoin = (CoinMarkets)(CurrenciesListAll.SelectedItem);
+
+                List<string> marketsNames = exchangesClient.GetExchangesList().Result.Select(m => m.Id).Take(5).ToList();
+
+                List<string> marketsLinks = new List<string>();
+                foreach(string name in marketsNames)
+                {
+                    marketsLinks.AddRange(exchangesClient.GetTickerByExchangeId(name, selectedCoin.Id).Result.Tickers.Select(t => t.TradeUrl).ToList());
+                }
+
+                Markets.ItemsSource = marketsLinks;
             }
         }
     }
