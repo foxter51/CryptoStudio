418681959bd0b79efb07c788e99acc5b4d035b3d
Ihor Fedorchenko <invisiblepanda04@gmail.com>
2023-01-21 10:37:42 +0200

    localization + themes fix

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 
@@ -7,6 +8,8 @@ namespace TestAssignment
     public partial class MainPage : Page
     {
         public static bool darkTheme = false;
+        public static bool englishLanguage = true;
+
         public MainPage()
         {
             InitializeComponent();
@@ -21,18 +24,20 @@ namespace TestAssignment
 
         private void Button_Click_1(object sender, System.Windows.RoutedEventArgs e)
         {
-
+            if (englishLanguage) englishLanguage = false;
+            else englishLanguage = true;
+            Switch_Language();
         }
 
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
             Switch_Theme();
+            Switch_Language();
         }
 
         private void Switch_Theme()
         {
             LinearGradientBrush linearGradientBrush = (LinearGradientBrush)Resources["LinearPanelBrush"];
-            SolidColorBrush labelsBrush = (SolidColorBrush)Resources["LabelsColor"];
             linearGradientBrush.GradientStops.Clear();
 
             if (darkTheme)
@@ -41,7 +46,7 @@ namespace TestAssignment
                 linearGradientBrush.GradientStops.Add(new GradientStop(Colors.Gray, 0.5));
                 linearGradientBrush.GradientStops.Add(new GradientStop(Colors.Black, 0.9));
                 MenuBar.Background = new SolidColorBrush(Colors.Gray);
-                labelsBrush.Color = Colors.White;
+                Resources["LabelsColor"] = Brushes.White;
             }
             else
             {
@@ -49,8 +54,25 @@ namespace TestAssignment
                 linearGradientBrush.GradientStops.Add(new GradientStop(Colors.White, 0.5));
                 linearGradientBrush.GradientStops.Add(new GradientStop(Colors.LightGoldenrodYellow, 0.9));
                 MenuBar.Background = new SolidColorBrush(Colors.BlanchedAlmond);
-                labelsBrush.Color = Colors.Black;
+                Resources["LabelsColor"] = Brushes.Black;
             }
+        }
+
+        private void Switch_Language()
+        {
+            this.Resources.MergedDictionaries[2].Clear();
+            var resourceDictionary = new ResourceDictionary();
+
+            if (englishLanguage)
+            {
+                resourceDictionary.Source = new Uri("/TestAssignment;component/Resources/lang.xaml", UriKind.RelativeOrAbsolute);
+            }
+            else
+            {
+                resourceDictionary.Source = new Uri("/TestAssignment;component/Resources/lang.ua-UA.xaml", UriKind.RelativeOrAbsolute);
+            }
+
+            this.Resources.MergedDictionaries.Add(resourceDictionary);
         }
     }
 }
--- PopularCurrencies.xaml.cs
+++ PopularCurrencies.xaml.cs
@@ -26,6 +26,7 @@ namespace TestAssignment
         private async void Page_Loaded(object sender, RoutedEventArgs e)
         {
             Switch_Theme();
+            Switch_Language();
             if((await pingClient.GetPingAsync()).GeckoSays != String.Empty)
             {
                 CurrenciesList.ItemsSource = searchClient.GetSearchTrending().Result.TrendingItems.Select(item => item.TrendingItem).ToList();
@@ -39,7 +40,6 @@ namespace TestAssignment
         private void Switch_Theme()
         {
             LinearGradientBrush linearGradientBrush = (LinearGradientBrush)Resources["LinearPanelBrush"];
-            SolidColorBrush labelsBrush = (SolidColorBrush)Resources["LabelsColor"];
             linearGradientBrush.GradientStops.Clear();
 
             if (MainPage.darkTheme)
@@ -47,15 +47,32 @@ namespace TestAssignment
                 linearGradientBrush.GradientStops.Add(new GradientStop(Colors.Black, 0.1));
                 linearGradientBrush.GradientStops.Add(new GradientStop(Colors.Gray, 0.5));
                 linearGradientBrush.GradientStops.Add(new GradientStop(Colors.Black, 0.9));
-                labelsBrush.Color = Colors.White;
+                Resources["LabelsColor"] = Brushes.White;
             }
             else
             {
                 linearGradientBrush.GradientStops.Add(new GradientStop(Colors.LightGoldenrodYellow, 0.1));
                 linearGradientBrush.GradientStops.Add(new GradientStop(Colors.White, 0.5));
                 linearGradientBrush.GradientStops.Add(new GradientStop(Colors.LightGoldenrodYellow, 0.9));
-                labelsBrush.Color = Colors.Black;
+                Resources["LabelsColor"] = Brushes.Black;
             }
+        }
+
+        private void Switch_Language()
+        {
+            this.Resources.MergedDictionaries[1].Clear();
+            var resourceDictionary = new ResourceDictionary();
+
+            if (MainPage.englishLanguage)
+            {
+                resourceDictionary.Source = new Uri("/TestAssignment;component/Resources/lang.xaml", UriKind.RelativeOrAbsolute);
+            }
+            else
+            {
+                resourceDictionary.Source = new Uri("/TestAssignment;component/Resources/lang.ua-UA.xaml", UriKind.RelativeOrAbsolute);
+            }
+
+            this.Resources.MergedDictionaries.Add(resourceDictionary);
         }
     }
 }
